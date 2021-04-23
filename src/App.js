b344c19093354ff3890ca8bf11dd270422987ad5
Danijel Demirovic <cucak92@gmail.com>
2021-04-23 15:19:55 +0200

    table and fetch functionality

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+import { Row, Container, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import TableView from "../src/Table/index";
+import logo from "../src/assets/codeus-logo.png";
+
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState({
+    from: 1,
+    to: 20,
+  });
+  const [items, setItems] = useState();
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const getTokenUrl = "https://f-test-02.glitch.me/data";
+    axios.get(getTokenUrl).then((res) => {
+      setToken(res.data.token);
+    });
+  }, []);
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    const name = evt.target.name;
+    
+    setState({
+      ...state,
+      [name]: value,
+    });
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    let rangeTo = parseInt(state.to);
+    let rangeFrom = parseInt(state.from);
+
+    let url =
+      "https://f-test-02.glitch.me/data?from=" +
+      rangeFrom +
+      "&to=" +
+      rangeTo +
+      "&token=" +
+      token;
+
+    axios.get(url).then((res) => {
+      setToken(res.data.token);
+      setItems(res.data.data);
+    });
+  }
+
+  return (
+    <div className="App">
+      <Row>
+        <Container>
+          <h1>Codeus App</h1>
+          <Form>
+            <Form.Group controlId="fromInput">
+              <Form.Label>From: </Form.Label>
+              <Form.Control
+                type="number"
+                name="from"
+                value={state.from}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="toInput">
+              <Form.Label>To: </Form.Label>
+              <Form.Control
+                type="number"
+                name="to"
+                value={state.to}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" onClick={handleClick}>
+              Load
+            </Button>
+          </Form>
+          {items ? <TableView items={items} /> : null}
+        </Container>
+      </Row>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Table/index.js
@@ -0,0 +1,32 @@
+import Table from "react-bootstrap/Table";
+
+function TableView({ items }) {
+  return (
+    <div id="table-view">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Slot</th>
+            <th>City</th>
+            <th>Velocity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, i) =>
+            item.slot && item.city && item.velocity ? (
+              <tr key={i}>
+                <td>{i}</td>
+                <td> {item.slot}</td>
+                <td>{item.city}</td>
+                <td>{item.velocity}</td>
+              </tr>
+            ) : null
+          )}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default TableView;
